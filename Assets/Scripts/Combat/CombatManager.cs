3950ad7e8a6c65187e867284212732244e73ef15
ethangilmore <ethanmckgilmore@gmail.com>
2024-03-25 11:00:12 -0600

    Made actions sequential in a turn

--- Assets/Scripts/Combat/CombatManager.cs
+++ Assets/Scripts/Combat/CombatManager.cs
@@ -6,46 +6,66 @@ using System.Linq;
 public class CombatManager : MonoBehaviour
 {
     [field: SerializeField] public float TurnDuration { get; private set; } = 5.0f;
+    [field: SerializeField] public float ActionTime { get; private set; } = 0.2f;
     public float TurnTime { get; private set; } = 0.0f;
     public float NormalizedTurnTime => TurnTime / TurnDuration;
 
     public CombatAgent Player;
     public List<CombatAgent> Enemies = new List<CombatAgent>();
     public List<CombatAgent> Agents { get => new List<CombatAgent> { Player }.Concat(Enemies).ToList(); }
+    private bool _processingTurn = false;
 
     private void Update()
     {
+        if (_processingTurn) return;
+        
         TurnTime += Time.deltaTime;
         if (TurnTime >= TurnDuration)
         {
             TurnTime = 0.0f;
-            ProcessTick();
+            StartCoroutine(ProcessTurn());
         }
     }
 
-    private void ProcessTick()
+    private IEnumerator ProcessTurn()
     {
-        List<Action> actions = new List<Action>();
+        _processingTurn = true;
+        Dictionary<CombatAgent, Action> actions = new Dictionary<CombatAgent, Action>();
         foreach (var agent in Agents)
         {
-            actions.Add(agent.GetNextAction());
+            actions.Add(agent, agent.GetNextAction());
         }
         // TODO: Buffs / Effects should be processed here
-        foreach (var action in actions)
+        if (actions[Player].Priority)
         {
-            if (action.Priority)
+            actions[Player].Animate();
+            yield return new WaitForSeconds(ActionTime);
+            actions[Player].Execute(this);
+        }
+        foreach (var enemy in Enemies)
+        {
+            if (actions[enemy].Priority)
             {
-                action.Execute(this);
-                action.Animate();
+                actions[enemy].Animate();
+                yield return new WaitForSeconds(ActionTime);
+                actions[enemy].Execute(this);
             }
         }
-        foreach (var action in actions)
+        if (!actions[Player].Priority)
         {
-            if (!action.Priority)
+            actions[Player].Animate();
+            yield return new WaitForSeconds(ActionTime);
+            actions[Player].Execute(this);
+        }
+        foreach (var enemy in Enemies)
+        {
+            if (!actions[enemy].Priority)
             {
-                action.Execute(this);
-                action.Animate();
+                actions[enemy].Animate();
+                yield return new WaitForSeconds(ActionTime);
+                actions[enemy].Execute(this);
             }
         }
+        _processingTurn = false;
     }
 }
