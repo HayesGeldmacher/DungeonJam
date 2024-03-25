fb417253cb21cc84bda21415caf0eb63a60fcb85
Hayes Geldmacher <hayes.g850@gmail.com>
2024-03-25 15:22:41 -0600

    More Work on Banner

--- Assets/Scripts/Combat/Banner/CombatBanner.cs
+++ Assets/Scripts/Combat/Banner/CombatBanner.cs
@@ -6,13 +6,15 @@ using TMPro;
 public class CombatBanner : MonoBehaviour
 {
     [SerializeField] private Animator _anim;
-    [SerializeField] private float _waitTime;
+    [SerializeField] private float _disappearWaitTime;
+    [SerializeField] private float _slideWaitTime;
     [SerializeField] private TextMeshProUGUI _infoText;
-
+    private Animator _parentAnim;
 
 
     private void Start()
     {
+        _parentAnim = GetComponent<Animator>();
         StartCoroutine(Appear("This is test string!"));
     }
 
@@ -20,7 +22,37 @@ public class CombatBanner : MonoBehaviour
     {
         _infoText.text = info;
         _anim.SetTrigger("appear");
-        yield return new WaitForSeconds(_waitTime);
+        yield return new WaitForSeconds(_slideWaitTime);
+        _parentAnim.SetTrigger("slide");
+        yield return new WaitForSeconds(_disappearWaitTime);
+        _anim.SetTrigger("disappear");
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+
+    public void CallPushDown()
+    {
+        StartCoroutine(PushDown());
+    }
+
+    public void CallDestroy()
+    {
+        StartCoroutine(Destroy());
+    }
+
+    private IEnumerator PushDown()
+    {
+        _parentAnim.ResetTrigger("slide");
+        _parentAnim.SetTrigger("slide");
+        _parentAnim.ResetTrigger("disappear");
+        _anim.SetTrigger("disappear");
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+
+    private IEnumerator Destroy()
+    {
+        _anim.ResetTrigger("disappear");
         _anim.SetTrigger("disappear");
         yield return new WaitForSeconds(1);
         Destroy(gameObject);
